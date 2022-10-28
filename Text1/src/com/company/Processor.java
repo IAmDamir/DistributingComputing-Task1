d9dcdacd252c0829d1010f4d1f966b9741bfa6bc
Damir <temirgalievdamir03@gmail.com>
2022-10-28 09:39:27 +0600

    initial commit

--- Text1/src/com/company/Processor.java
+++ Text1/src/com/company/Processor.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 /**
  * Processor of HTTP request.
  */
-public class Processor {
+public class Processor implements Runnable {
     private final Socket socket;
     private final HttpRequest request;
     private PrintWriter output;
@@ -20,11 +20,16 @@ public class Processor {
         this.request = request;
     }
 
-    public void process() throws IOException {
-        // Print request that we received.
-        System.out.println("Got request:");
-        System.out.println(request.toString());
+    @Override
+    public void run() {
+        try {
+            process();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void process() throws IOException {
         output = new PrintWriter(socket.getOutputStream());
 
         output.println("HTTP/1.1 200 OK");
@@ -32,27 +37,25 @@ public class Processor {
         output.println();
 
         String[] arr = request.getRequestLine().split(" ");
-        if (arr[0] != null) {
-            if (Objects.equals(arr[0], "GET")) {
-                if (arr[1] != null) {
-                    String[] reqArr = Arrays.stream(arr[1].split("/"))
-                            .filter(s -> (s != null && s.length() > 0))
-                            .toArray(String[]::new);
+        String[] reqArr = Arrays.stream(arr[1].split("/"))
+                .filter(s -> (s != null && s.length() > 0))
+                .toArray(String[]::new);
 
-                    if (reqArr.length > 0) {
-                        String action = reqArr[0];
-                        if (Objects.equals(action, "exec")) {
-                            HandleTask(reqArr);
-                        }
-                    } else {
-                        output.println("<html>");
-                        output.println("<head><title>Hello</title></head>");
-                        output.println("<body><p>Hello, world!</p></body>");
-                        output.println("</html>");
-                    }
-                }
+        if (arr[0] == null) { return; }
+        if (!Objects.equals(arr[0], "GET")) { return; }
+
+        if (reqArr.length > 0) {
+            String action = reqArr[0];
+            if (Objects.equals(action, "exec")) {
+                HandleTask(reqArr);
             }
+        } else {
+            output.println("<html>");
+            output.println("<head><title>Hello</title></head>");
+            output.println("<body><p>Hello, world!</p></body>");
+            output.println("</html>");
         }
+
         output.flush();
         System.out.flush();
 
